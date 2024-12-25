b95bcef24ad9254d4b269c7e12207248f682d99e
matheus.antonio <matheusk42@hotmail.com>
2024-12-25 20:37:39 -0300

    Ajustando o exercicio

--- exercicio_pi.py
+++ exercicio_pi.py
@@ -6,7 +6,7 @@ raio_do_circulo = input("Digite o raio: ")
 
 try:
     raio_do_circulo = float(raio_do_circulo)
-    area_do_circulo =  round(2 * pi * raio_do_circulo,2)
+    area_do_circulo =  round(pi * raio_do_circulo ** 2,2)
     print(f'A área do círculo é: {area_do_circulo}')
 except:
     print("Tipagem do dado está incorreta")
