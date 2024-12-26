20a180a71f5e5b9128edbb26a86850556496d6d8
matheus.antonio <matheusk42@hotmail.com>
2024-12-25 22:23:31 -0300

    Salvando o 15 exer´cicico

---
+++ exercicio_15.py
@@ -0,0 +1,8 @@
+#Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+var1 = input("Digite a primeira palavra: ")
+var2 = input("Digite a segunda palavra: ")
+
+palavra = var1 + " " + var2
+
+print(palavra)
