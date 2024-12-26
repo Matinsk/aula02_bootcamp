0a954578159ebbf861cfd100ea44a5f2916c86eb
matheus.antonio <matheusk42@hotmail.com>
2024-12-25 22:14:55 -0300

    Salvando o 11 exercicio

---
+++ exercicio_11.py
@@ -0,0 +1,5 @@
+#Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+texto = input("Digite uma palavra: ")
+
+print(f'o texto em máisculo ficará assim :{texto.upper()}')
