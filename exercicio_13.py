b1ec9ecd49b8ad384b5709b499f01d60d94dd203
matheus.antonio <matheusk42@hotmail.com>
2024-12-25 22:19:18 -0300

    Salvando o 13 exercício

---
+++ exercicio_13.py
@@ -0,0 +1,7 @@
+#Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+frase = input("Digite uma frase: ")
+
+frase_formatada = frase.split()
+
+print(frase_formatada)
